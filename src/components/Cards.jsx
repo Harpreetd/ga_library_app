d27186d8ec9b9e49ea739f5916474f51866fff2f
Renatebh <renate.hem@gmail.com>
2022-09-08 14:20:20 +0200

    added new pages to project

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,10 +9,7 @@ export const Cards = () => {
           return (
             <div key={key} className="card-container">
               <div className="card">
-                <img
-                  src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                  width="150px"
-                />
+                <img src={data.image} />
                 <h2>{data.title}</h2>
 
                 <p>{data.authorName}</p>
--- src/jsonData/bookList.js
+++ src/jsonData/bookList.js
@@ -1,16 +1,24 @@
+import book1 from "../assets/image/book.jpg";
+
 const booklist = [
   {
+    image: book1,
     id: ["101", "102", "103", "104"],
     title: "Harry Potter",
     isbn: "43876",
     year: "2001",
     edition: "second",
 
-    studyLine: ["salg og ledelse", "Prosjektledelse", "digital markedsforing"],
+    studyLine: {
+      courseOne: "digital markedsføring",
+      courseTwo: "salg, ledelse og markedsføring",
+      courseThree: "prosjektledelse",
+    },
 
     authorName: "J.K.Rowlings",
   },
   {
+    image: book1,
     id: ["201", "202", "203", "204"],
     title: "It",
     isbn: "43876",
@@ -22,6 +30,7 @@ const booklist = [
     authorName: "Per",
   },
   {
+    image: book1,
     id: ["301", "302", "303", "304"],
     title: "C++",
     isbn: "43876",
