fc2dc4281d77f83ff71c3d595942688e82acf96c
Harpreetd <dhimanharpreet17@yahoo.in>
2022-10-05 12:43:52 +0200

    basic dropdown structure

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Dropdown = () => {
-  return <div>Dropdown</div>;
+  return (
+    <div>
+      <select name="courseLine" id="courseLine">
+        <option value="frontend">Front end</option>
+        <option value="backend">Back end</option>
+        <option value="salg">Salg, ledelse og markedsføring</option>
+        <option value="digital">Digital markedsføring</option>
+        <option value="prosjekt">Prosjekt Ledelse</option>
+      </select>
+    </div>
+  );
 };
 
 export default Dropdown;
